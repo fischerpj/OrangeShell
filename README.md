f495ac69a019c459d1bd6c1a98f6728073a63408
granpiero <fischer.pj@gmail.com>
2024-07-03 10:43:30 +0000

    2426

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
-echo "# OrangeShell" >> README.md
+echo "\# OrangeShell" \>\> README.md
+
 git init
+
 git add README.md
+
 git commit -m "first commit"
+
 git branch -M main
+
 git remote add origin https://github.com/fischerpj/OrangeShell.git
+
 git push -u origin main
 
 and so on
--- posts/qmd_romans.sh
+++ posts/qmd_romans.sh
@@ -4,19 +4,26 @@ book="rom"
 start=1
 end=16
 chap=1
+leading_zero="0"
+regex='^[0-9]$'
 
 for ((ndx=$start;ndx<=$end;ndx++))
 do
+if [[ $ndx =~ $regex ]]
+then prefixed_ndx=$leading_zero$ndx
+else prefixed_ndx=$ndx
+fi
 ref=$book$ndx
-filename=$ref.qmd
+filename=$book$prefixed_ndx.qmd
+echo $prefixed_ndx
 echo $filename
 cat > $filename <<EOF
 ---
-title: $title_prefix$ndx
+title: $title_prefix$prefixed_ndx
 author: 'paul'
 date: '2024-06-24'
 categories: [SG21]
-tags: [$book]
+tags: [$title_prefix]
 params:
   refs: $ref
   refs_after: Rom1:1
