f237c5f698475e53cc7ac6525ad9c071338d1d74
granpiero <fischer.pj@gmail.com>
2024-07-08 11:00:55 +0000

    2426

---
+++ dd.R
@@ -0,0 +1,15 @@
+<script src="https://giscus.app/client.js"
+data-repo="Fischerpj/xminotes"
+data-repo-id="R_kgDOMSdMLQ"
+data-category="Announcements"
+data-category-id="DIC_kwDOMSdMLc4CgmMV"
+data-mapping="pathname"
+data-strict="0"
+data-reactions-enabled="1"
+data-emit-metadata="0"
+data-input-position="bottom"
+data-theme="preferred_color_scheme"
+data-lang="fr"
+crossorigin="anonymous"
+async>
+  </script>
---
+++ posts/john_nco/qmd_jas.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+title_prefix="jacques_"
+book="jas"
+end=5
+start=1
+chap=1
+leading_zero="0"
+regex='^[0-9]$'
+offset=5
+
+for ((ndx=$start;ndx<=$end;ndx++))
+do
+if [[ $ndx =~ $regex ]]
+then prefixed_ndx=$leading_zero$ndx
+else prefixed_ndx=$ndx
+fi
+ref=$book$ndx
+filename=$book$prefixed_ndx.qmd
+echo $prefixed_ndx
+echo $filename
+cat > $filename <<EOF
+---
+title: $title_prefix$prefixed_ndx
+author: 'paul'
+date: now
+date-format: "D MMM YY HH:mm.ss"
+categories: [SG21]
+tags: [$title_prefix]
+params:
+  refs: $ref
+  refs_after: Rom1:1
+  version: SG21
+---
+
+\`\`\`{r, BCV_code, echo=FALSE}
+XMINPI::bcv_list_(params\$refs, params\$version)
+\`\`\`
+
+EOF
+
+done
